152e6277aad485c5d1b52f6bcdd5df52f6451039
Steve Konves <stephen.konves@gmail.com>
2016-11-05 13:37:13 -0700

    Enable debugging See https://gist.github.com/dchowitz/83bdd807b5fa016775f98065b381ca4e

---
+++ .compiled/index.js
@@ -0,0 +1,7 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+const x = { a: 'a', b: 'b' };
+const c = 'c';
+
+console.log(_extends({}, x, { c }));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2luZGV4LmpzIl0sIm5hbWVzIjpbIngiLCJhIiwiYiIsImMiLCJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiOztBQUNBLE1BQU1BLElBQUksRUFBRUMsR0FBRyxHQUFMLEVBQVVDLEdBQUcsR0FBYixFQUFWO0FBQ0EsTUFBTUMsSUFBSSxHQUFWOztBQUVBQyxRQUFRQyxHQUFSLGNBQWdCTCxDQUFoQixJQUFtQkcsQ0FBbkIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJcbmNvbnN0IHggPSB7IGE6ICdhJywgYjogJ2InIH07XG5jb25zdCBjID0gJ2MnXG5cbmNvbnNvbGUubG9nKHsuLi54LCBjfSk7XG4iXX0=
