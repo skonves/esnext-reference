67d60cfde3e64e41c91943b8f222d1c0cb017c25
Steve Konves <stephen.konves@gmail.com>
2016-11-07 08:19:00 -0700

    more rules

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import { func } from './worker';
 func();
 
 const x = { a: 'a', b: 'b' };
-const c = 'c'
+const c = 'c';
 
-show({...x, c});
+const asdf = { c, 'f-f': x };
 
-function show({a, b, c, d}) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-    console.log(d);
+asdf['f-f'];
+asdf.c;
+
+show({ ...x, c });
+
+if (true) {
+	asdf.c = 'asdf';
 }
+
+var t = d => { return x; };
+
+const asdfasdf = [ 4, 5, 6, 7 ];
+
+const show = function ({ a, b, c, d }) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+	console.log(d);
+};
